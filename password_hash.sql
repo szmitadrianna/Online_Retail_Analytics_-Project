2647f66f8395bc0046ca0b40496577ed0e31a856
Adrianna <adrianna.konczalska@gmail.com>
2025-06-04 17:27:45 +0200

    Add files via upload
    In the table *users*, user passwords are stored as secure hashes rather than plain text.For the purpose of this portfolio project, sample password hashes have been generated manually.

---
+++ password_hash.sql
@@ -0,0 +1,6 @@
+SELECT SHA2('adminadmin2233', 256);
+SELECT SHA2('johnsmith123', 256);
+SELECT SHA2('monicamonica99', 256);
+SELECT SHA2('annagarcia11', 256);
+SELECT SHA2('annakowalsky89', 256);
+SELECT SHA2('johnwashington54', 256);
