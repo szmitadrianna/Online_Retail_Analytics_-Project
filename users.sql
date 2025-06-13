337254c80597059c9a2833248ca48c2abcf58988
Adrianna <adrianna.konczalska@gmail.com>
2025-06-13 14:53:53 +0200

    Add files via upload

---
+++ users.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (username, user_role, email_address, password_hash, 
+phone_number, registration_date, is_active)
+VALUES 
+('annasmith', 'admin', 'admin@example.com', 
+'c6d386372490e443b51b6a5683be64fd057e5496bd52d6d37d5ea1889bc6b7f3',
+'543234986', '2024-01-01', 1),
+('johnsmith', 'csr', 'johnsmith@example.com', 
+'b93f79668a4fbb52632c10f09aa87ec9d354276bf737a60e9c51d60bf382d435',
+'514876222', '2024-01-01', 1),
+('monicagonzales', 'csr', 'monicagonzales@example.com', 
+'c8f10643a7b82145518ab5f735aa9b6d1103e6b809a838a8f2f20f874463cf69', 
+'653176543', '2024-01-02', 1),
+('annagarcia', 'csr', 'annagarcia@example.com', 
+'c8f10643a7b82145518ab5f735aa9b6d1103e6b809a838a8f2f20f874463cf69',
+'876222654', '2024-01-02', 1),
+('annakowalsky', 'wh', 'annakowalsky@example.com', 'd8a298646ad859108d2e052bf34a9b1f50c65e1e2109553aa47dadb278ebbe3f',
+'765333567','2024-01-02', 1),
+('johnwashington', 'wh', 'jwashington@example.com', '4ed14dbbc59a72fc1ff1b86f7c8453a7ad4c1a03bf298a239843f067ac718451',
+'765234000', '2024-01-03', 0);
