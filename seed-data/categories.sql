f06f180fbe37e67a5718f20f270a2089ddee3d40
Adrianna <adrianna.konczalska@gmail.com>
2025-06-21 15:13:34 +0200

    Add files via upload

---
+++ seed-data/categories.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM gymglamour_shop.categories
+LIMIT 0, 50
+
+-- Date: 2025-06-21 15:12
+*/
+INSERT INTO `` (`id_category`,`category_name`,`category_description`,`created_date`,`is_active`) VALUES (1,'leggings','very tight trousers made from a material that stretches easily','2024-01-05',1);
+INSERT INTO `` (`id_category`,`category_name`,`category_description`,`created_date`,`is_active`) VALUES (2,'bras','a piece of underwear that supports the breasts','2024-01-01',1);
+INSERT INTO `` (`id_category`,`category_name`,`category_description`,`created_date`,`is_active`) VALUES (3,'shorts','trousers that end above the knee or reach the knee, often worn in hot weather or when playing a sport','2024-01-06',1);
+INSERT INTO `` (`id_category`,`category_name`,`category_description`,`created_date`,`is_active`) VALUES (4,'tshirts','piece of clothing, usually with short sleeves','2024-01-08',1);
+INSERT INTO `` (`id_category`,`category_name`,`category_description`,`created_date`,`is_active`) VALUES (5,'accessories','hats&bags','2024-01-10',0);
